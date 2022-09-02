34ccc2c7c1d411f7a78a176ccc4a3e11d5c1f90f
Modane <danielnegri2002@gmail.com>
2022-09-02 17:38:01 -0300

    Atualização da NavBar (responsiva): * Reorganização de elementos para aparência mais próxima da real; * Adição de menu responsivo de slide; * Alteração da fonte do button da nav; * Adição de botão colapse com efeito (v. Gambiarra).

--- src/components/patterns/LogoNavBar/index.js
+++ src/components/patterns/LogoNavBar/index.js
@@ -12,12 +12,14 @@ export default function LogoNavBar({logoSrc, logoAlt, backgroundColor, textColor
     classes = classes.trim();
     //#endregion
 
-    console.log("Render");
+    
     //Seta o estado de affix da nav
     const [isAffix, setIsAffix] = useState(false);
 
     return ( 
         <nav id="NavBar" className={classes}>
+            
+            <Logo src={logoSrc} alt={logoAlt} href="/" objectPosition="left center" height="100%" width="150px"/>
             
             <button onClick={()=>{//Implementação gambiarra de um button pra criar o efeito affix
                 const nav = document.getElementById("NavBar");
@@ -30,13 +32,13 @@ export default function LogoNavBar({logoSrc, logoAlt, backgroundColor, textColor
                 setIsAffix(!isAffix);
             }}>On</button>
 
-            <Logo src={logoSrc} alt={logoAlt} href="/" objectPosition="left center" height="100%" width="150px"/>
             <div id="nav" className={styles.nav_field}>
                 <ButtonLink 
                     href={`https://wa.me/+55${whatsapp}`}
                     target="_blank"
                     color={isAffix ? textColor : backgroundColor}
                     background={"transparent"}
+                    fontsize="lg"
                 >
                     WhatsApp
                 </ButtonLink>
@@ -45,10 +47,21 @@ export default function LogoNavBar({logoSrc, logoAlt, backgroundColor, textColor
                     target="_blank"
                     color={isAffix ? textColor : backgroundColor}
                     background={"transparent"}
+                    fontsize="lg"
                 >
                     ({telefone.substring(0,2)}) {telefone.substring(2,telefone.length-4)}-{telefone.substring(telefone.length, telefone.length-4)}
                 </ButtonLink>
             </div>
+
+            <button className={styles.colapse} onClick={()=>{//Implementação gambiarra de um button pra colapse
+                const nav = document.getElementById("nav");
+                if(!nav.classList.contains(styles.active)){
+                    nav.classList.add(styles.active);
+                }
+                else{
+                    nav.classList.remove(styles.active);
+                }
+            }}>[?]</button>
         </nav>
      );
 }
