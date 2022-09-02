ef33c93a5ad878f7bf310a3c7bc73ed74c74502c
Modane <danielnegri2002@gmail.com>
2022-09-02 15:11:29 -0300

    Alteração de estilos da navbar. * Background do button removido; * Alteração de cor do texto; * Inserção de espaçamento do button da nav; * Adição de detalhamento de texto onHover na nav.

--- src/components/patterns/LogoNavBar/index.js
+++ src/components/patterns/LogoNavBar/index.js
@@ -1,4 +1,4 @@
-import React, { Children } from 'react';
+import React, { useState } from 'react';
 import Logo from '../../cores/Logo';
 import ButtonLink from '../../elements/ButtonLink'; //TOFIX
 import styles from './LogoNavBar.styles.module.scss';
@@ -12,12 +12,22 @@ export default function LogoNavBar({logoSrc, logoAlt, backgroundColor, textColor
     classes = classes.trim();
     //#endregion
 
+    console.log("Render");
+    //Seta o estado de affix da nav
+    const [isAffix, setIsAffix] = useState(false);
+
     return ( 
         <nav id="NavBar" className={classes}>
             
             <button onClick={()=>{//Implementação gambiarra de um button pra criar o efeito affix
                 const nav = document.getElementById("NavBar");
-                !nav.classList.contains(styles.affix) ? nav.classList.add(styles.affix) : nav.classList.remove(styles.affix);
+                if(!nav.classList.contains(styles.affix)){
+                    nav.classList.add(styles.affix);
+                }
+                else{
+                    nav.classList.remove(styles.affix);
+                }
+                setIsAffix(!isAffix);
             }}>On</button>
 
             <Logo src={logoSrc} alt={logoAlt} href="/" objectPosition="left center" height="100%" width="150px"/>
@@ -25,14 +35,16 @@ export default function LogoNavBar({logoSrc, logoAlt, backgroundColor, textColor
                 <ButtonLink 
                     href={`https://wa.me/+55${whatsapp}`}
                     target="_blank"
-                    color={textColor}
+                    color={isAffix ? textColor : backgroundColor}
+                    background={"transparent"}
                 >
                     WhatsApp
                 </ButtonLink>
                 <ButtonLink 
                     href={`tel:55${telefone}`}
                     target="_blank"
-                    color={textColor}
+                    color={isAffix ? textColor : backgroundColor}
+                    background={"transparent"}
                 >
                     ({telefone.substring(0,2)}) {telefone.substring(2,telefone.length-4)}-{telefone.substring(telefone.length, telefone.length-4)}
                 </ButtonLink>
